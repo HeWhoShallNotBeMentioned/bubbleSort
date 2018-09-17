246ee25ffdfad249cef31d4fd0561b1a8fa24772
Andrew Ferguson <andrewmf@umich.edu>
2018-09-17 13:59:31 -0500

    BubbleSort complete

--- bubblesort.js
+++ bubblesort.js
@@ -1,6 +1,4 @@
 
-let array = [1, 2, 3];
-
 function swap(array, index){
   if (array[index + 1] < array[index]){
    [array[index], array[index + 1]] = [array[index + 1], array[index]];
@@ -9,7 +7,22 @@ function swap(array, index){
 }
 
 function bubbleSort(array) {
+  console.log(`before ${array}`);
+  // for(let i = 0; i < array.length - 1; i++) {
+  //   for(let j = 0; j < array.length - 1; j++) {
+  //     console.log(`inner ${array}`);
+  //     swap(array, j);
+  //   }
+  //   console.log(`outer ${array}`);
 
+  let counter = 0;
+  while (counter < array.length - 1) {
+    for(let i = 0; i < array.length - 1; i++) {
+    swap(array, i);
+    }
+    counter++;
+  }
+  return array;
 }
 
 
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,25 +1,27 @@
 describe('BubbleSort Tests', function () {
-  beforeAll(function () {
-  // let array = [1, 2, 3];
-  //  array.swap = swap();
-
-  spyOn(window, 'swap').and.callThrough();
-
+  beforeEach(function () {
+    spyOn(window, 'swap').and.callThrough();
   });
 
-  it('swap was called', function () {
+  it('swap spy is on', function () {
     expect(window.swap.calls.count()).toEqual(0);
   });
-    it('handles an empty array', function () {
-      expect(bubbleSort([])).toEqual([]);
-    });
 
-    it('handles an array with one element', function () {
-      expect(bubbleSort([1])).toEqual([1]);
-    });
+  it('handles an empty array', function () {
+    expect(bubbleSort([])).toEqual([]);
+  });
 
-    it('handles an array with multiple elements', function () {
-      expect(bubbleSort([2, 1])).toEqual([1, 2]);
-      expect(bubbleSort([0, -1, 42, 23, -4, 7])).toEqual([-4, -1, 0, 7, 23, 42]);
-    });
+  it('handles an array with one element', function () {
+    expect(bubbleSort([1])).toEqual([1]);
+  });
+
+  it('handles an array with multiple elements', function () {
+    expect(bubbleSort([2, 1])).toEqual([1, 2]);
+    expect(window.swap.calls.count()).toEqual(1);
+  });
+
+  it('Extra', function () {
+    expect(bubbleSort([0, -1, 42, 23, -4, 7])).toEqual([-4, -1, 0, 7, 23, 42]);
+    expect(window.swap.calls.count()).toEqual(25);
+  });
 });
