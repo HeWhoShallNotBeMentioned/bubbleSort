4c688d60f95f396ca5267f80ca6775deed71d09c
Andrew Ferguson <andrewmf@umich.edu>
2018-09-17 11:47:01 -0500

    working on spec file

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,28 +1,25 @@
-beforeAll(function () {
+describe('BubbleSort Tests', function () {
+  beforeAll(function () {
+  // let array = [1, 2, 3];
+  //  array.swap = swap();
+
+  it('swap was called', function () {
+    expect(array.swap.calls.count()).toEqual();
+  });
+
   spyOn(array, 'swap').and.callThrough(); // replace existing `tootsiepop['lick']` method
-});
-it('getting to the center of tootsiepop involves exactly three licks', function () {
-  array.bubbleSort();
-  expect(bubbleSort.swap.calls.count()).toEqual(3);
-});
-
-
-describe('Bubble Sort', function(){
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
   });
-});
 
-describe('Bubble Sort', function () {
-  it('handles an array with one element', function () {
-    expect(bubbleSort([1])).toEqual([1]);
-  });
-});
+    it('handles an empty array', function () {
+      expect(bubbleSort([])).toEqual([]);
+    });
 
-describe('Bubble Sort', function () {
-  it('handles an array with multiple elements', function () {
-    expect(bubbleSort([2,1])).toEqual([1,2]);
-    expect(bubbleSort([0,-1,42,23,-4,7])).toEqual([-4,-1,0,7,23,42]);
-  });
-});
+    it('handles an array with one element', function () {
+      expect(bubbleSort([1])).toEqual([1]);
+    });
 
+    it('handles an array with multiple elements', function () {
+      expect(bubbleSort([2, 1])).toEqual([1, 2]);
+      expect(bubbleSort([0, -1, 42, 23, -4, 7])).toEqual([-4, -1, 0, 7, 23, 42]);
+    });
+});
